ac5dfb70e57fc712eec3de5d9b171f4d5de150ea
abatuhan06 <abatuhan0666@gmail.com>
2023-05-12 22:52:16 +0300

    added odev3

---
+++ gazi login page/app/src/main/java/dev/batuhansener/examples/loginpage/MainActivity.java
@@ -0,0 +1,17 @@
+package dev.batuhansener.examples.loginpage;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ActionBar bar = getSupportActionBar();
+        bar.hide();
+    }
+}
